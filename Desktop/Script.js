f71792963a01ebe1d912fc4671d5f8fe033d8599
eLeven <kimlong11082004@gmail.com>
2025-01-16 16:45:30 +0700

    This is comit message

---
+++ Desktop/Script.js
@@ -0,0 +1,11 @@
+document.querySelector('.component-1').addEventListener('click', function() {
+    document.getElementById('dichvu').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.component').addEventListener('click', function() {
+    document.getElementById('nav').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.component-2').addEventListener('click', function() {
+    document.getElementById('sanpham').scrollIntoView({ behavior: 'smooth' });
+});
